b014bf389cde2ea04a5c4ca703bd08f36c6a3331
Jean Carlo Loza Carbajal <jeancarloloza@hotmail.com>
2020-02-22 09:44:07 -0600

    v0 test

---
+++ Observador/Observador/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Observador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
